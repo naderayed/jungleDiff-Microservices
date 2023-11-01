791fe5b512ba55a9485dd1b358733cedcca5ae4a
naderayed <nader.ayed@esprit.tn>
2023-11-01 03:26:52 +0100

    adding docker file

--- articles/src/test/java/com/junglediff/articles/ArticlesApplicationTests.java
+++ articles/src/test/java/com/junglediff/articles/ArticlesApplicationTests.java
@@ -3,10 +3,10 @@ package com.junglediff.articles;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class ArticlesApplicationTests {
 
-	@Test
+
 	void contextLoads() {
 	}
 
