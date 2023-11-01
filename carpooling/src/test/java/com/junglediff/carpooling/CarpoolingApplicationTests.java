b139fd2ff38b6eb4a28795c633ef4849e9056747
naderayed <nader.ayed@esprit.tn>
2023-11-01 03:47:27 +0100

    adding dockerfile2

--- carpooling/src/test/java/com/junglediff/carpooling/CarpoolingApplicationTests.java
+++ carpooling/src/test/java/com/junglediff/carpooling/CarpoolingApplicationTests.java
@@ -3,10 +3,10 @@ package com.junglediff.carpooling;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class CarpoolingApplicationTests {
 
-	@Test
+
 	void contextLoads() {
 	}
 
